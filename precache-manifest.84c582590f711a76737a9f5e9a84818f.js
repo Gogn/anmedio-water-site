c9fee6a7463a01fa1cce37d7fe93dede226b19b0
Dero <106393@gmail.com>
2020-02-24 11:53:39 +0400

    Updates

---
+++ precache-manifest.84c582590f711a76737a9f5e9a84818f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8a994e2d13106491aabe666fe07e1d",
+    "url": "/anmedio-water-site/index.html"
+  },
+  {
+    "revision": "155b3e7e0db3ac54989a",
+    "url": "/anmedio-water-site/static/css/main.fd32538a.chunk.css"
+  },
+  {
+    "revision": "093ac6b9b0ac1fdfdf3d",
+    "url": "/anmedio-water-site/static/js/2.d9063c7d.chunk.js"
+  },
+  {
+    "revision": "831142632ac8f65e27536d916cf72f8f",
+    "url": "/anmedio-water-site/static/js/2.d9063c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155b3e7e0db3ac54989a",
+    "url": "/anmedio-water-site/static/js/main.8f111da2.chunk.js"
+  },
+  {
+    "revision": "817065d099250f025a92",
+    "url": "/anmedio-water-site/static/js/runtime-main.23657a53.js"
+  },
+  {
+    "revision": "15e9ba7bc902a5368223038ca0bca948",
+    "url": "/anmedio-water-site/static/media/CeraPro-Bold.15e9ba7b.ttf"
+  },
+  {
+    "revision": "7237787245f471af155652d9a8fc9526",
+    "url": "/anmedio-water-site/static/media/CeraPro-Bold.72377872.woff"
+  },
+  {
+    "revision": "f9bac5cd373511f7b8b02862984933c8",
+    "url": "/anmedio-water-site/static/media/CeraPro-Bold.f9bac5cd.woff2"
+  },
+  {
+    "revision": "2558fd3ae2d244ceab53b6b455731490",
+    "url": "/anmedio-water-site/static/media/CeraPro-Regular.2558fd3a.woff"
+  },
+  {
+    "revision": "bd818dce28b0e42919636be4fd356e51",
+    "url": "/anmedio-water-site/static/media/CeraPro-Regular.bd818dce.woff2"
+  },
+  {
+    "revision": "cf954f65e33513f8a1f75f8083674834",
+    "url": "/anmedio-water-site/static/media/CeraPro-Regular.cf954f65.ttf"
+  },
+  {
+    "revision": "5a376dfa90cf81dbf27b85824df1e90c",
+    "url": "/anmedio-water-site/static/media/back.5a376dfa.svg"
+  },
+  {
+    "revision": "e2aae19e745b829b89afe17ea1dce535",
+    "url": "/anmedio-water-site/static/media/back.e2aae19e.svg"
+  },
+  {
+    "revision": "e2b098860737ce02cdd0a95fb03994f2",
+    "url": "/anmedio-water-site/static/media/checkbox-ok.e2b09886.svg"
+  },
+  {
+    "revision": "76c7cfbb000f7803c6eb590875137628",
+    "url": "/anmedio-water-site/static/media/forward.76c7cfbb.svg"
+  },
+  {
+    "revision": "694c6e517af6a4f632f7cf39209d1b68",
+    "url": "/anmedio-water-site/static/media/ok.694c6e51.svg"
+  }
+]);
